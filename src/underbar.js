7d560f73a0a3d559312a94b168325ce8b6f42810
Nathan Houle <nathan@nathanhoule.com>
2013-10-11 16:37:38 -0700

    Minor grammar tweak

--- src/underbar.js
+++ src/underbar.js
@@ -5,7 +5,7 @@ var _ = {};
 (function() {
 
   // Returns whatever value is passed as the argument. This function doesn't
-  // seem very useful, but remember it--when a function needs to provide an
+  // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
     /* SOLUTION */
