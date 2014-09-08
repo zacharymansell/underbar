837ad5221571179c355ebcda63e9cdf69f9a90ba
Magee Mooney <magee@hackreactor.com>
2014-09-08 15:08:35 -0700

    adds missing close paren, etc. line 27 that causes part1.js spec to fail

--- spec/part1.js
+++ spec/part1.js
@@ -25,6 +25,7 @@
 
       it('should return empty array if zero is passed in as the index', function() {
         expect(_.first([1,2,3], 0)).to.eql([]);
+      });
 
       it('should return all the array\'s elements if the index argument is larger than the length of the array', function() {
         expect(_.first([1,2,3], 5)).to.eql([1, 2, 3]);
