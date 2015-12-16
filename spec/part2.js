6c5cbd077459ea3e68ec81679b6f8c885e694ec4
Dan Thareja <danthareja@gmail.com>
2015-12-16 14:57:53 -0800

    Add tests from _.once assessment prompt

--- spec/part2.js
+++ spec/part2.js
@@ -357,16 +357,46 @@
         });
       });
 
-      it('should only run a user-defined function if it hasn\'t been run before', function() {
+      it('should be a function', function() {
+        expect(_.once).to.be.an.instanceOf(Function);
+      });
+
+      it('should return a function', function() {
+        // noop is short for `no-operation` and is pronounced `no-op`
+        var noop = _.once(function() {});
+
+        expect(noop).to.be.an.instanceOf(Function);
+      })
+
+      it('should only run a user-defined function if it has not been run before', function() {
         var num = 0;
         var increment = _.once(function() {
-          num += 1;
+          num++;
         });
 
         increment();
         increment();
+        increment();
 
         expect(num).to.equal(1);
+      });
+
+      it('should apply arguments to the user-defined function', function() {
+        var add = _.once(function(x,y,z) {
+          return x + y + z;
+        });
+
+        expect(add(1, 2, 3)).to.equal(6);
+      });
+
+      it('should return the result of the first call for every subsequent call', function() {
+        var add = _.once(function(x,y,z) {
+          return x + y + z;
+        });
+
+        expect(add(1,2,3)).to.equal(6);
+        expect(add(4,5,6)).to.equal(6);
+        expect(add(7,8,9)).to.equal(6);
       });
     });
 
