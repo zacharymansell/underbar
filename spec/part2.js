519b2fcc6019ff023cfd633d45bd092d3fb716fd
Dan Thareja <danthareja@gmail.com>
2015-12-16 14:32:13 -0800

    Add tests from _.contains assessment prompt

--- spec/part2.js
+++ spec/part2.js
@@ -8,18 +8,63 @@
         _.contains([4, 5, 6], 2);
       });
 
-      it('should return false if a collection does not contain a user-specified value', function() {
-        expect(_.contains([4, 5, 6], 2)).to.be.false;
+      it('should be a function', function() {
+        expect(_.contains).to.be.an.instanceOf(Function);
       });
 
-      it('should return true if a collection contains a user-specified value', function() {
-        expect(_.contains([4, 5, 6], 5)).to.be.true;
+      it('should not mutate the input array', function() {
+        var input = [1,2,3,4,5];
+        var result = _.contains(input, 4);
+
+        /*
+         * Mutation of inputs should be avoided without good justification otherwise
+         * as it can often lead to hard to find bugs and confusing code!
+         * Imagine we were reading the code above, and we added the following line:
+         *
+         * var lastElement = input[input.length - 1];
+         *
+         * Without knowing that mutation occured inside of reduceRight,
+         * we would assume that `lastElement` is 5. But if inside of
+         * reduceRight, we use the array method `pop`, we would permanently
+         * change `input` and our assumption would not longer be true,
+         * `lastElement` would be 4 instead!
+         *
+         * The tricky part is that we have no way of knowing about the mutation
+         * just by looking at the code above. We'd have to dive into the
+         * implementation of reduceRight to the exact line that uses `pop`.
+         * If we write a lot of code with this assumption, it might be very hard
+         * to trace back to the correct line in reduceRight.
+         *
+         * You can avoid an entire class of bugs by writing functions
+         * that don't mutate their inputs!
+         */
+
+        expect(input).to.eql([1,2,3,4,5])
       });
 
-      it('should work on objects', function(){
-        expect(_.contains({ a: 4, b: 5, c: 6 }, 5)).to.be.true;
+      it('should return true given an array and a value from that array', function(){
+        var array = [1,2,3];
+        var value = 1;
+        expect(_.contains(array, value)).to.be.true;
       });
 
+      it('should return false given an array and a value not in that array', function(){
+        var array = [1,2,3];
+        var value = 4;
+        expect(_.contains(array, value)).to.be.false;
+      });
+
+      it('should return true given a object and a value from that object', function(){
+        var object = { a: 1, b: 2, c: 3 };
+        var value = 1;
+        expect(_.contains(object, value)).to.be.true;
+      });
+
+      it('should return false given an object and a value not in that object', function(){
+        var object = { a: 1, b: 2, c: 3 };
+        var value = 4;
+        expect(_.contains(object, value)).to.be.false;
+      });
     });
 
     describe('every', function() {
